c8177c96071e018a7b41f588c9e9d3ecb059cf83
rashid4807 <65720433+rashid4807@users.noreply.github.com>
2025-04-29 07:45:32 +0300

    Added header and footer file, started designing using bootstrap

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { Container } from 'react-bootstrap';
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <Container>
+        <h1>Welcome to the website</h1>
+        </Container>
+       
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
